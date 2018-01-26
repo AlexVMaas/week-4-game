fd42cb677fa5077f65748cdc03acbb3e967268f1
Alexander Maas <alex@MacBook-Pro-2.local>
2018-01-25 20:20:57 -0700

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
-    // On Page Load:
-        // Create variables (and set them to default values):
-	    	// computerRandomNumber
+
             var randomNumber;
 			// array of crystals values
             var crystalValues=[1,2,3,4];
@@ -11,15 +9,29 @@
 	        // userTotalScore
             var totalScore=0;
 
-        // Call reset function
-		// Call update items on the page function
+
     // On crystal click:
         // Figure out what crystal they clicked and store in a variable
         // Store crystal value in variable (crystalClickedValue)
         // Add crystalClickedValue to userTotalScore
         // Call update items on the page function
 
-        $("#crystals").on("click", )
+        $(".crystals").on("click", function() {
+            var index = parseInt($(this).attr("value"))
+            if (randomNumber === totalScore) {
+                wins++
+                reset()
+                update()
+            } else if (totalScore > randomNumber) {
+                losses++
+                reset ()
+                update ()
+            } else if (totalScore < randomNumber) {
+                totalScore += crystalValues[index]
+                update ()
+            }
+        })
+
         // if they win (userTotalScore === computerRandomNumber)
             // Add 1 to wins
             // Call reset function
@@ -28,22 +40,13 @@
 			// Add 1 to losses
 			// Call reset function
 			// Call update items on the page function
-    // Update items on the page (function):
-        // Put wins on page
-        // Put losses on page
-        // Put computerRandomNumber on page
-        // Put userTotalScore on page
+
         function update() {
             $("#wins").text(wins);
             $("#losses").text(losses);
             $("#random-number").text(randomNumber);
             $("#total-score").text(totalScore);
         }
-
-	// Reset (function):
-		// userTotalScore = 0
-        // computerRandomNumber = new random number between 19 and 120
-        // array of crystals values (new random numbers between 1 and 12)
 
         function reset() {
             totalScore = 0;
